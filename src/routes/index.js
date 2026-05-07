2f2cb3eb41ff3864e1507ea2d0637336775738d5
Regis Laguna <regissilva.adv@outlook.com>
2026-05-07 18:08:48 -0300

    atualização rotas teste

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,16 +17,15 @@ routes.post('/login', UsuarioController.login);
 routes.get('/servicos', ServicoController.index);
 routes.post('/agendamentos', AgendamentoController.store);
 routes.post('/questionario', QuestionarioController.store);
-
-// ATENÇÃO AQUI: Verifique se o seu Frontend envia a foto com o nome 'image' ou 'foto'.
-// Se for 'foto', mude para upload.single('foto')
 routes.post('/servicos', upload.single('image'), ServicoController.store); 
-routes.put('/servicos/:id', upload.single('image'), ServicoController.update);
 
 // --- Rotas Privadas (Admin) ---
 routes.use(authMiddleware); 
 
+// ATENÇÃO AQUI: Verifique se o seu Frontend envia a foto com o nome 'image' ou 'foto'.
+// Se for 'foto', mude para upload.single('foto')
 
+routes.put('/servicos/:id', upload.single('image'), ServicoController.update);
 routes.delete('/servicos/:id', ServicoController.delete);
 
 routes.get('/agendamentos-admin', AgendamentoController.index);
